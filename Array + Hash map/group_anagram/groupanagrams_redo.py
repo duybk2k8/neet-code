ce3027d25b793f8f9e3cfafc9b7ec5616ae569b5
duy <duybuikhanh5a0@gmail.com>
2026-06-26 11:05:10 +0700

    new exercise

---
+++ Array + Hash map/group_anagram/groupanagrams_redo.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+strs=input().split()
+res = defaultdict(list) #mapping charCount to list of Anagrams
+for s in strs:
+    count = [0] * 26 # a ... z
+    #tạo mảng 26 phần tử, đại diện 26 chữ cái a-z
+    #ý tưởng: key là số lần suất hiện từng chữ cái
+    #vdu: "tea" => "a" "e" "t" = [1,0,0,0,1,0...,0,1]
+    for c in s:
+        count[ord(c) - ord("a")] += 1
+        #ord là lấy giá trị ascii: vdu: a=80 b=81 ==> map a 0 => 80-80 map b 1 => 81 - 80
+        res[tuple(count)].append(s) #count is list => mutable => tuple
+print (list(res.values()))
